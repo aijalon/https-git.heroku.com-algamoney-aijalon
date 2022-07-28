ce06396d7ebe017ee8c36a287a368e68685a5b5f
Aijalon Júnior <juniorsjs6@hotmail.com>
2022-07-28 00:26:34 -0300

    Aula 3.10 completa

--- src/main/java/com/algaworks/algamoney/api/model/Categoria.java
+++ src/main/java/com/algaworks/algamoney/api/model/Categoria.java
@@ -7,6 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "categoria")
@@ -15,6 +17,9 @@ public class Categoria {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+	
+	@NotNull
+	@Size(min = 3, max =50)
 	private String nome;
 	
 	public Long getCodigo() {
